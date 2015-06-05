4d36877e2ac413800a9fe6d740dbffce9503aa9c
Andrés Zorro <zorrodg@gmail.com>
2015-06-05 07:21:42 -0500

    Mocking browser

--- _test/ajax-test.js
+++ _test/ajax-test.js
@@ -1,10 +1,15 @@
 import assert from 'assert';
+import Mc from 'mock-browser';
 import ajax from './../_src/helpers/ajax';
 
+let mock = new Mc.mocks.MockBrowser();
+
 describe('Ajax Helper test', () => {
+  let document = mock.getDocument(),
+      div = document.createElement('div');
 
   describe('Init', () => {
-    it('Should be a function', () => assert.equal(typeof ajax, 'function'))
+    it('Should be a function', () => assert.equal(typeof ajax, 'function'));
   });
 
 });
--- _test/editcss-test.js
+++ _test/editcss-test.js
@@ -1,10 +1,15 @@
 import assert from 'assert';
+import Mc from 'mock-browser';
 import editcss from './../_src/helpers/editcss';
 
+let mock = new Mc.mocks.MockBrowser();
+
 describe('EditCSS Helper test', () => {
+  let document = mock.getDocument(),
+      div = document.createElement('div');
 
   describe('Init', () => {
-    it('Should be a function', () => assert.equal(typeof editcss, 'function'))
+    it('Should be a function', () => assert.equal(typeof editcss, 'function'));
   });
 
 });
